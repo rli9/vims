ddd3ec173a02d9e7207e98e7c93d206a61bf9689
rli9 <ruijia.li@gmail.com>
2015-06-26 12:04:07 +0800

    upload secret token to repo
    Signed-off-by: rli9 <ruijia.li@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vims::Application.config.secret_key_base = '811bf8a34a89f24729aa87429cdf0f2a382dc65d246944643464e51ac2ffaa38f0b98e1a86e1605b98df92742a03e1da541b1c310964c57c9d4ef882542bf524'
